0ce6b9cce2e7aca62a797cb84f3b6a67e8837aa3
megi-messer <heinze.obach@web.de>
2026-05-18 11:26:26 +0200

    new feature

--- claude-finance-analyzer/kurs/kurs.py
+++ claude-finance-analyzer/kurs/kurs.py
@@ -7,12 +7,20 @@ kürzel = ticker_format(importer("finance.xlsx"))
 #print(kürzel)
 
 
-
+"""
 def finance_info(kürzel):
+    analyse_list = []
     test = kürzel
-    return test
+    for i in kürzel:
 
-print(finance_info(kürzel))
+
+    return analyse_list
+
+
+"""
+    
+
+
 
 
 #def Tickerinfo():
@@ -21,9 +29,28 @@ print(finance_info(kürzel))
 
 
 
-#day = yf.Ticker("XXlkjölkjölk")
-#print(day.info)
-#print(day.history(period='1mo'))
+dat = yf.Ticker("AAPL")
+print(dat.info)
+print(dat.history(period='1mo'))
 
 #tickers = yf.Tickers('MSFT AAPL GOOG')
 #yf.download(['MSFT', 'AAPL', 'GOOG'], period='1mo')
+
+ticker = yf.Ticker("AAPL")
+info = ticker.info
+
+relevant = {
+    "name": info.get("shortName"),
+    "sector": info.get("sector"),
+    "current_price": info.get("currentPrice"),
+    "52w_high": info.get("fiftyTwoWeekHigh"),
+    "52w_low": info.get("fiftyTwoWeekLow"),
+    "pe_ratio": info.get("trailingPE"),
+    "dividend_yield": info.get("dividendYield"),
+    "recommendation": info.get("recommendationKey"),
+    "analyst_target": info.get("targetMeanPrice"),
+    "beta": info.get("beta"),
+    "market_cap": info.get("marketCap"),
+}
+
+print(relevant)
