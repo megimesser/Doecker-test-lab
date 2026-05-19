716998e4de37c1a71136210dfd61365f6fa30c33
megi-messer <heinze.obach@web.de>
2026-05-19 09:29:06 +0200

    new api function

--- claude-finance-analyzer/kurs/kurs.py
+++ claude-finance-analyzer/kurs/kurs.py
@@ -7,26 +7,44 @@ kürzel = ticker_format(importer("finance.xlsx"))
 #print(kürzel)
 
 
-"""
+
 def finance_info(kürzel):
+    global analyse_list
     analyse_list = []
-    test = kürzel
     for i in kürzel:
+        ticker = yf.Ticker(i)
+        #print(dat.history(period='1mo'))
+        info = ticker.info
+
+        relevant = {
+            "name": info.get("shortName"),
+            "sector": info.get("sector"),
+            "current_price": info.get("currentPrice"),
+            "52w_high": info.get("fiftyTwoWeekHigh"),
+            "52w_low": info.get("fiftyTwoWeekLow"),
+            "pe_ratio": info.get("trailingPE"),
+            "dividend_yield": info.get("dividendYield"),
+            "recommendation": info.get("recommendationKey"),
+            "analyst_target": info.get("targetMeanPrice"),
+            "beta": info.get("beta"),
+            "market_cap": info.get("marketCap"),
+        }
+
+        analyse_list.append(relevant)
 
 
     return analyse_list
 
 
+finance_info(kürzel)
+
+print(analyse_list)
+
+
 """
-    
-
-
-
 
 #def Tickerinfo():
 #test
-
-
 
 
 dat = yf.Ticker("AAPL")
@@ -36,21 +54,4 @@ print(dat.history(period='1mo'))
 #tickers = yf.Tickers('MSFT AAPL GOOG')
 #yf.download(['MSFT', 'AAPL', 'GOOG'], period='1mo')
 
-ticker = yf.Ticker("AAPL")
-info = ticker.info
-
-relevant = {
-    "name": info.get("shortName"),
-    "sector": info.get("sector"),
-    "current_price": info.get("currentPrice"),
-    "52w_high": info.get("fiftyTwoWeekHigh"),
-    "52w_low": info.get("fiftyTwoWeekLow"),
-    "pe_ratio": info.get("trailingPE"),
-    "dividend_yield": info.get("dividendYield"),
-    "recommendation": info.get("recommendationKey"),
-    "analyst_target": info.get("targetMeanPrice"),
-    "beta": info.get("beta"),
-    "market_cap": info.get("marketCap"),
-}
-
-print(relevant)
+"""
