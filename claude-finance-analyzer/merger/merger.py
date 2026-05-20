34f3c14ab3a2c01fe30ca6139c97ab7525d7a49f
megi-messer <heinze.obach@web.de>
2026-05-20 21:18:29 +0200

    new json function

--- claude-finance-analyzer/merger/merger.py
+++ claude-finance-analyzer/merger/merger.py
@@ -8,11 +8,57 @@ def json_reader(path):
 
 
 test = importer("finance.xlsx")
+json_path = "finance.json"
 formator= format_portfolio(test)
 
-def merger(formator):
+#print(formator)
+
+
+with open(json_path, "r") as f:
+        data = json.load(f)
+
+#print(data)
+
+
+for i in data:
+     print(i["ticker"])
+     
+     
+
+
+
+
+#print(type(formator))
+
+for i in formator:
+    for l in i:
+        for s in data:
+             if s["ticker"] == l:
+                  print("gefunden")
+
+
+
+
+
+
+def merger(formator, json_path):
+    format = formator
+    with open(json_path, "r") as f:
+        data = json.load(f)
+
+    #for key, value in data:
+      #  key 
+        
 
     return 
 
 
 print(json_reader(json_path))
+
+
+if __name__ == "__main__":
+    #df = importer("finance.xlsx")
+    #print(df.columns.tolist())
+    #print(format_portfolio(df))
+    print(json_reader(json_path))
+   
