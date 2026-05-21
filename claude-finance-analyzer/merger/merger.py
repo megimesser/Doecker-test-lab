f7114f176ac4a18375fa4b208fa98981493f4ebb
megi-messer <heinze.obach@web.de>
2026-05-21 16:01:04 +0200

    test

--- claude-finance-analyzer/merger/merger.py
+++ claude-finance-analyzer/merger/merger.py
@@ -34,11 +34,17 @@ with open(json_path, "r") as f:
     data = json.load(f)
 
 # 2. Für jeden API-Eintrag den passenden Excel-Eintrag finden
+
+#Iteration durch die API Json
 for stock in data:
+    # Iteration durch die Excel
     for excel_row in formator:
+        # Wenn der Ticker der Excel die der Json matcht
         if stock["ticker"] in excel_row:
+            # Iteration durch die Exceldictionarys nach dem Value
             for value in excel_row:
                 if isinstance(value, float):
+                    #Zugriff auf Eigenkapitalfeld 
                     stock["Eigenkapital"] = value
                     break
 
@@ -46,7 +52,7 @@ for stock in data:
 with open(json_path, "w") as f:
     json.dump(data, f, indent=4, ensure_ascii=False)
 
-
+#test
 
 
 
