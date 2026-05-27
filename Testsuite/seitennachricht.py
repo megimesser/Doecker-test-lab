db23c33a46c7e2d8d30979efd08a5438329595c0
megi-messer <heinze.obach@web.de>
2026-05-27 12:56:34 +0200

    working on selenium automation

--- Testsuite/seitennachricht.py
+++ Testsuite/seitennachricht.py
@@ -1,33 +1,52 @@
 from selenium import webdriver
-import time
-import requests
-import os
-from dotenv import load_dotenv
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import time
 
-
-
+test_mail = "anything@uvvtkqpz.mailosaur.net"
+testnummer = "01602986882"
+message = "Dies ist eine automatisch generierte Nachricht zur Testautomation"
 
 driver = webdriver.Chrome(
     service=Service(ChromeDriverManager().install())
 )
 
+def aussteller(test_mail, testnummer, message):
+    driver.get("https://www.wohnbautrend.de")
+
+    print(driver.title)
+
+    driver.implicitly_wait(10)
+
+    name = driver.find_element(By.CSS_SELECTOR, ".w-input")
+    mail = driver.find_element(By.CSS_SELECTOR, ".text-field-2.w-input")
+    nummer = driver.find_element(By.CSS_SELECTOR, ".spacer-form.w-input")
+    nachricht = driver.find_element(By.ID, "Ihre-Nachricht")
+    confirm = driver.find_element(By.ID, "checkbox-2")
+    dropdown = driver.find_element(By.ID, "Sie-sind")
+    submit = driver.find_element(By.CSS_SELECTOR, ".submit-button.w-button")
 
 
-driver.get("https://www.wohnbautrend.de")
+    name.send_keys("Testautomation")
+    mail.send_keys(test_mail)
+    nummer.send_keys(testnummer)
+    select = Select(dropdown)
+    select.select_by_visible_text("Option 1")
+    nachricht.send_keys(message)
+    confirm.click()
+    submit.click()
+    time.sleep(5)
 
-title = driver.title
-print(title)
-
-field_1 = driver.find_element(by=By.CLASS_NAME, value="w-input")
-field_1.send_keys("Selenium")
-#submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
 
 
-time.sleep(30)
 
-driver.implicitly_wait(100)
+
+
+
+### Function callingd ### 
+
+if __name__ == "__main__":
+    aussteller(test_mail,testnummer,message)
 
