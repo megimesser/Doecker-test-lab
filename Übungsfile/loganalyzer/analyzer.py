cc7a7e90a3ae49e85899c8fbc9a3dbfddf3df775
megi-messer <heinze.obach@web.de>
2026-06-15 16:40:29 +0200

    test

--- Übungsfile/loganalyzer/analyzer.py
+++ Übungsfile/loganalyzer/analyzer.py
@@ -2,6 +2,9 @@ import os
 from config import *
 import re
 import json
+import argparse
+
+
 
 
 
@@ -18,24 +21,80 @@ jsoni = {
 }
 
 
+parser = argparse.ArgumentParser(description="Analysiert app.log")
+# Parser ist ein Objekt und wird mit den Instanzvariablen instanziiert
+
+
+parser.add_argument(
+      "--level",
+      help="nur dieses Log_Level zählen"
+      )
+
+parser.add_argument(
+      "--user",
+      help="nur Messages dieses Users zählen"
+)
+
+parser.add_argument(
+      "--output",
+      help="Outputpfad des Files"
+      
+)
+
+#Was macht parser.add
+# Parser - objekt
+# add_argument - Objectmethod aus der argparse class
+
+args = parser.parse_args()
+# Was macht parse_args()?
+#Liest die Argumente aus der Kommandozeile (sys.argv)
+#Prüft sie anhand der Definitionen aus add_argument()
+#Wandelt sie ggf. in die angegebenen Typen um (int, float, ...)
+#Speichert sie in einem Objekt (Namespace)
+#Gibt dieses Objekt zurück
+
+
+
+
+
+
+
 for lines in log:
         
         jsoni["gesamt"] += 1
         
         #Definition Message
+        
         info = re.findall("INFO", lines)
         error = re.findall("ERROR", lines)
         warning = re.findall("WARNING", lines)
         critical = re.findall("CRITICAL", lines)
 
-        if bool(info) == True: 
+        if bool(info) == True and args.level == "INFO": 
               jsoni["nach_level"]["INFO"] += 1
-        elif bool(error) == True:
+              jsoni["nach_level"]["ERROR"] = None
+              jsoni["nach_level"]["WARNING"] = None
+              jsoni["nach_level"]["CRITICAL"] = None
+        elif bool(error) == True and args.level == "ERROR":
               jsoni["nach_level"]["ERROR"] += 1
-        elif bool(warning) == True:
+              jsoni["nach_level"]["WARNING"] = None
+              jsoni["nach_level"]["CRITICAL"] = None
+              jsoni["nach_level"]["INFO"] = None
+        elif bool(warning) == True and args.level == "WARNING":
               jsoni["nach_level"]["WARNING"] += 1
-        elif bool(critical) == True:
+              jsoni["nach_level"]["CRITICAL"] = None
+              jsoni["nach_level"]["INFO"] = None
+              jsoni["nach_level"]["ERROR"] = None
+        elif bool(critical) == True and args.level == "CRITICAL":
               jsoni["nach_level"]["CRITICAL"] += 1
+              jsoni["nach_level"]["INFO"] = None
+              jsoni["nach_level"]["ERROR"] = None
+              jsoni["nach_level"]["WARNING"] = None
+        else: 
+              jsoni["nach_level"]["CRITICAL"] += 1
+              jsoni["nach_level"]["WARNING"] += 1
+              jsoni["nach_level"]["ERROR"] += 1
+              jsoni["nach_level"]["INFO"] += 1
         
               
         #Definition Namen
@@ -44,12 +103,12 @@ for lines in log:
         Peter = re.findall("Peter", lines)
         Günther = re.findall("Günther", lines)
 
-
-        if bool(Max) == True: 
+# Problem die erste Schleife springt nur an wenn ein Level angegeben wird 
+        if bool(Max) == True and args.user == "Max": 
               jsoni["nach_user"]["Max"] += 1
-        elif bool(Laura) == True:
+        elif bool(Laura) == True and args.user == "Laura":
               jsoni["nach_user"]["Laura"] += 1
-        elif bool(Peter) == True:
+        elif bool(Peter) == True and args.user == "Peter":
               jsoni["nach_user"]["Peter"] += 1
         elif bool(Günther) == True:
               jsoni["nach_user"]["Günther"] += 1
@@ -68,7 +127,10 @@ for lines in log:
                 jsoni["erster_critical"] = stringer
                 #print(stringer)
 
-        
+if args.user:
+            print(f"{jsoni["nach_user"][args.user]}")
+if args.level: 
+            print(f"{jsoni["nach_level"][args.level]}")
               
 
 
